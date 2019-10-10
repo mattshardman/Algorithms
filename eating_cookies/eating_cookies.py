b7846b62aabdaa82f56a45e1a938b0f236a5a12d
mattshardman <mattshardman@gmail.com>
2019-10-10 17:22:47 +0100

    add memoization

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,13 +5,22 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookies(n, cache={}):
   if n == 0:
     return 1
+
   if n <= 2:
     return n
-  
-  return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+  if cache and cache.get(n) > 0:
+    return cache[n]
+
+  if not cache:
+    for i in range(n + 1):
+      cache[i] = 0
+
+  cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
