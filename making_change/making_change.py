09a85c5c7899cb927b4d093193564d54f6be15ad
mattshardman <mattshardman@gmail.com>
2019-10-11 15:21:10 +0100

    add memoization

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,6 +2,34 @@
 
 import sys
 
+def making_change_memo(amount, denominations, cache={}):
+  if amount == 0:
+    return 1
+
+  if amount < 0:
+    return 0
+
+  if cache and cache.get(amount) > 0:
+    return cache[amount]
+
+  if not cache:
+    for i in range(amount + 1):
+      cache[amount] = 0
+
+  combinations = 0
+  for coin in range(len(denominations)):
+    if denominations[coin] > amount:
+      break 
+    combinations += making_change_memo(amount - denominations[coin], denominations[coin:], cache)
+  
+  cache[amount] = combinations
+  return cache[amount]
+
+denominations = [1, 5, 10, 20, 50]
+
+x = making_change_memo(10, denominations)
+print(x)
+
 def making_change(amount, denominations):
   if amount == 0:
     return 1
@@ -17,10 +45,8 @@ def making_change(amount, denominations):
   
   return combinations
 
-denominations = [1, 5, 10, 20, 50]
-
-x = making_change(10, denominations)
-
+y = making_change(10, denominations)
+print(y)
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
